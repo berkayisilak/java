f54010b9f111be12fb43b8f4cacedbbb0a85764e
Berkay Işılak <101112697+berkayisilak@users.noreply.github.com>
2023-04-26 23:57:25 +0300

    exercises

---
+++ src/Triangle.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Triangle {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num;
+
+        //Taking digit number from user
+        System.out.print("Basamak sayısını giriniz: ");
+        num = scan.nextInt();
+
+        for (int i = 0; i < num; i++) {
+
+            //Fill in the blanks
+            for (int j = 0; j < num - i; j++) {
+                System.out.print(" ");
+            }
+
+            //Fill the stars
+            for (int k = 0; k <= (2 * i); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
